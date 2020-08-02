f28d1c9231c0ac831e863dfc19d8e59a7191d1f2
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-08-02 21:07:26 +0300

    add initial javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
-(document).ready(function(){
-  ("#name").click(function(){
-    ("#1").click(alert("You have chosen Magaritta Pizza"))
-  })
-})
+
+  var small,medium,large;
+  var crustPrices=[200,150,100];
+  var toppings=[mushroom,onion,bacon];
+  var sizes=[small,medium,large];
+  
+  function myPrice() {
+    var size=document.getElementById("size").value
+    var crust=document.getElementById("crust").value
+    var topping=document.getElementById("topping").value
+  }
+  
