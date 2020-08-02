963aa7f9a8fa2e78ffd6478f0b121bb85d70bf15
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-08-02 22:32:43 +0300

    add medium as constant and introduce a division to output text

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,31 @@
 
   var small,medium,large;
-  var crustPrices=[200,150,100];
-  var toppings=[mushroom,onion,bacon];
+  var crustPrices=[200,150,100]
   var sizes=[small,medium,large];
+  var sizePrices=[1000,850,600]
+  var toppingPrices=[50,50,50]
   
   function myPrice() {
-    var size=document.getElementById("size").value
-    var crust=document.getElementById("crust").value
-    var topping=document.getElementById("topping").value
+  var inputSize=document.getElementById("size").value;
+  var inputCrust=document.getElementById("crust").value;
+  var inputTopping=document.getElementById("topping").value;
+
+  if (inputSize=="large" && inputCrust=="crispy" && inputTopping=="mushroom") {
+    document.getElementById("result").innerHTML=("Your price should be " + parseInt(sizePrices[0] + toppingPrices[0]+crustPrices[0]));
   }
+  else if (inputSize=="large" && inputCrust=="stuffed" && inputTopping=="mushroom") {
+    document.getElementById("result").innerHTML=("Your price should be " + parseInt(sizePrices[0] + toppingPrices[0]+crustPrices[1]))
+  }
+  else if (inputSize=="large" && inputCrust=="gluten-free" && inputTopping=="mushroom") {
+    document.getElementById("result").innerHTML=("Your price should be " + parseInt(sizePrices[0] + toppingPrices[0]+crustPrices[2]))
+  }
+  else if (inputSize=="medium" && inputCrust=="crispy" && inputTopping=="mushroom") {
+    document.getElementById("result").innerHTML=("Your price should be " + parseInt(sizePrices[1] + toppingPrices[0]+crustPrices[0]))
+  }
+  else if (inputSize=="medium" && inputCrust=="stuffed" && inputTopping=="mushroom") {
+    document.getElementById("result").innerHTML= ("Your price should be " + parseInt(sizePrices[1] + toppingPrices[0]+crustPrices[1]))
+  }
+  }
+ 
+
   
